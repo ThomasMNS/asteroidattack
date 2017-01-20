abf6accf1083c7762836160175816b33bf82bf4c
ThomasMNS <thomasmns@outlook.com>
2017-01-20 14:20:49 +0000

    Added slider for voume (not functional)

--- settings_scene.py
+++ settings_scene.py
@@ -13,12 +13,25 @@ class SettingsScene(generic_scene.GenericScene):
     def __init__(self):
         super().__init__()
 
+        # Buttons
         self.return_button = ui_items.RectangleHoverButton("Return", 300, 90, 362, 640, constants.LIGHT_GREY,
                                                            constants.DARK_GREY)
         self.buttons = [self.return_button]
 
+        # UI elements
+        self.slider = ui_items.Slider(100, 250, 200, 0, 100, 50)
+
+        # Text
+        self.header_font = pygame.font.Font(None, 45)
+        self.header_render = self.header_font.render("Audio Settings", True, constants.WHITE)
+        self.header_rect = self.header_render.get_rect()
+
+        self.font = pygame.font.Font(None, 25)
+        self.volume_render = self.font.render("Sound volume", True, constants.WHITE)
+
     def handle_events(self, events):
         for event in events:
+            self.slider.handle_events(event)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.return_button.mouse_over is True:
                     self.next_scene = ui_scenes.TitleScene()
@@ -27,9 +40,15 @@ class SettingsScene(generic_scene.GenericScene):
         for button in self.buttons:
             button.mouse_on_button(pygame.mouse.get_pos())
 
+        self.slider.update()
+
     def draw(self, screen):
         # Background
         screen.fill(constants.DARKER_GREY)
 
         for button in self.buttons:
             button.draw_button(screen)
+
+        screen.blit(self.header_render, ((1024 / 2) - (self.header_rect.width / 2), 40))
+        screen.blit(self.volume_render, (100, 200))
+        self.slider.draw(screen)
